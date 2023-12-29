0b0c179143d1cbbd8d67d0ed556142bb30ec82a9
BrianZ <briannlongzhao@gmail.com>
2023-12-29 13:39:13 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding=utf-8
-# Copyright 2023 The HuggingFace Inc. team. All rights reserved.
+# Original Copyright 2023 The HuggingFace Inc. team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,6 +12,11 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
+#
+# Changes made:  
+# - Date: 11/01/2023
+# - Author: Brian Nlong Zhao, Yuhang Xiao  
+# - Description: Modified training pipeline for compatibility with DreamDistribution
 
 import argparse
 import time
